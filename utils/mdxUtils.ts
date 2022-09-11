8f523d7bd54780f91365df69e7684cf91526214d
nick-kal <kalomoiris.nikolaos@gmail.com>
2022-09-12 02:54:43 +0300

    Deploy

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -5,6 +5,7 @@ import path from "path";
 export const POSTS_PATH = path.join(process.cwd(), "pages/posts");
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
+
 export const postFilePaths = fs
   .readdirSync(POSTS_PATH)
   // Only include md(x) files
